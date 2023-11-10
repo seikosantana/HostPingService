1734079c1823e11890588b43f864f9b000e764f5
Seiko Santana <santanaaseiko@gmail.com>
2023-11-10 10:50:32 +0700

    Add EventLog

--- HostPingerService/Program.cs
+++ HostPingerService/Program.cs
@@ -3,19 +3,31 @@
 using HostPingerService;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.EventLog;
 
-IHostBuilder builder =  Host.CreateDefaultBuilder(args);
+IHostBuilder builder = Host.CreateDefaultBuilder(args);
 builder.UseContentRoot(AppContext.BaseDirectory);
 if (OperatingSystem.IsWindows())
 {
     builder.UseWindowsService();
 }
 
-builder.ConfigureServices(services =>
-    {
-        services.AddHostedService<PingerService>();
-    }
+builder.ConfigureServices(services => { services.AddHostedService<PingerService>(); }
 );
+
+builder.ConfigureLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.AddConsole();
+    if (OperatingSystem.IsWindows())
+    {
+        logging.AddEventLog(new EventLogSettings()
+        {
+            SourceName = "HostPinger"
+        });
+    }
+});
 
 IHost app = builder.Build();
 await app.RunAsync();
